9df04da8fb1806c9aea19a8ce22ce64fe3c4ad3e
daler_khusainov <dalerkhusainov85@gmail.com>
2022-09-18 01:49:16 +0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,8 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e0a6dc4c-3d61-45f4-afe0-30ee0290257b';
+export const MODAL_CLOSE_SEC = 2.5
+
+
+
